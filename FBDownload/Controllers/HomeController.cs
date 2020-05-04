3ea39bfde8bece63f9dc311d07ba4f84c54a0f32
River Phoenix <river_phoenixx@outlook.com>
2020-05-04 17:56:53 -0500

    First version commit solution

--- FBDownload/Controllers/HomeController.cs
+++ FBDownload/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace FBDownload.Controllers
 {
@@ -13,18 +14,55 @@ namespace FBDownload.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [ValidateInput(false)]
+        public string GetFBVideo(string info)
         {
-            ViewBag.Message = "Your application description page.";
+            string urls = GetFBURL(info);
 
-            return View();
+            return urls;
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        string GetFBURL(string info)
+        {
+            string jsonURLS = string.Empty;
+
+            if (info.IndexOf("sd_src:") > 0)
+            {
+                string sdVideoInfo = info.Substring(info.IndexOf("sd_src:"));
+                string hdVideoInfo = info.Substring(info.IndexOf("hd_src:"));
+
+                string urlSD = sdVideoInfo.Substring(0, sdVideoInfo.IndexOf(","));
+                string urlHD = hdVideoInfo.Substring(0, hdVideoInfo.IndexOf(","));
+
+                string sd = urlSD.Substring(urlSD.IndexOf(":") + 1).Replace("\"", "");
+                string hd = urlHD.Substring(urlHD.IndexOf(":") + 1).Replace("\"", "");
+
+                jsonURLS = new JavaScriptSerializer().Serialize(new
+                {
+                    sd,
+                    hd
+                });
+            }
+            else if (info.IndexOf("property=\"og:video:url\" content=\"") > 0)
+            {
+                string sdVideoInfo = info.Substring(info.IndexOf("property=\"og:video:url\" content=\""));
+                sdVideoInfo = sdVideoInfo.Substring(sdVideoInfo.IndexOf("https"));
+                string urlSD = sdVideoInfo = sdVideoInfo.Substring(0, sdVideoInfo.IndexOf("\""));
+
+                urlSD = Server.HtmlDecode(urlSD.Replace("video.fmex12-1.fna.fbcdn.net", "video.xx.fbcdn.net"));
+
+                jsonURLS = new JavaScriptSerializer().Serialize(new
+                {
+                    sd = urlSD,
+                    hd = "null"
+                });
+            }
+            else
+                throw new Exception();
+
+            return jsonURLS;
         }
+
     }
 }
